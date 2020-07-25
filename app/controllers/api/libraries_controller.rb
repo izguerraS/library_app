2bc98c9dfadc1a3955bce8325586bd1a5011db5f
Sam <Izguerra.sam@gmail.com>
2020-07-25 00:31:42 -0500

    index & show working

--- app/controllers/api/libraries_controller.rb
+++ app/controllers/api/libraries_controller.rb
@@ -1,2 +1,13 @@
 class Api::LibrariesController < ApplicationController
+
+  def index
+    @books = Book.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+
+    render 'show.json.jb'
+  end
 end
